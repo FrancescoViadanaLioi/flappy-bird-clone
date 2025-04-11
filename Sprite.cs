de8bd0a10836d79e7c9723cc458ae5fa070a6ea6
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-04-11 09:00:48 -0300

    Primeiro commit do projeto Flappy Bird

---
+++ Sprite.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public class Sprite : Godot.Sprite
+{
+
+
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public override void _Process(float delta)
+    {
+        
+    }
+}
